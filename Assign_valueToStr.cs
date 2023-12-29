08c1b6494beb41b562079e7ce4f9709c34fc2538
Vikas Umbadwar <vikasumbadwar@gmail.com>
2023-12-29 16:01:19 +0530

    4th solved

---
+++ Assign_valueToStr.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {   string empName = "puja";
+        Console.WriteLine(empName);
+
+
+        Console.ReadLine();
+    }
+}
