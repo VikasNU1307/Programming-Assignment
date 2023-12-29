842526c4e90bac3554ec853f107cdefebbddedd2
Vikas Umbadwar <vikasumbadwar@gmail.com>
2023-12-29 16:16:24 +0530

    1 solved

---
+++ 2.ReadandWrite/1.print.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string empName = "vikas";
+        Console.WriteLine("The Name is:"+empName);
+
+
+        Console.ReadLine();
+    }
+}
