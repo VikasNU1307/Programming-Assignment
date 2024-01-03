819e0c5e323de47746227ff7b23b887de92f1081
Vikas Umbadwar <vikasumbadwar@gmail.com>
2024-01-03 20:40:21 +0530

    3(3)

---
+++ 3.BuiltInDataTypes/3.ConvertFloatToDecimal.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Assignment33
+{
+    public void ConvertFloatToDecimal()
+    {
+         float value = 2.11F;
+         int num =(int) value;
+         Console.WriteLine(num);
+
+    }
+}
