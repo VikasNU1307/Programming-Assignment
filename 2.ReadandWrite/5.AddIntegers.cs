60942bb2d52dd65575956f8cbc02ed63fd062f09
Vikas Umbadwar <vikasumbadwar@gmail.com>
2024-01-03 19:32:05 +0530

    5

---
+++ 2.ReadandWrite/5.AddIntegers.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Xml.XPath;
+
+class Assignment
+{
+    public int  AddIntegers()
+    {
+       int num1 = 0, num2 = 0;
+       num1 = int.Parse(Console.ReadLine());
+       num2 = int .Parse(Console.ReadLine());
+       return num1 + num2;
+
+    }
+}
