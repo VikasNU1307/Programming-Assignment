fcd8bddd8c7d8e6405b22d9ee03da1f10be2f803
Vikas Umbadwar <vikasumbadwar@gmail.com>
2024-01-03 19:24:52 +0530

    4

---
+++ 2.ReadandWrite/4.ConcatenateStrings.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Assignment4
+{
+    public void ConcatenateStrings()
+    {
+      string fname, lname;
+      fname =   Console.ReadLine();
+      lname =   Console.ReadLine();
+      Console.Write("Hello "+fname+" " +lname);
+    }
+}
