95577657cc1bcee68aacd14ebb40eed06b1c3fa4
Vikas Umbadwar <vikasumbadwar@gmail.com>
2024-01-05 20:27:19 +0530

    4(1)

---
+++ 4.StringDataType/1.ReverseStringWithoutReverseFunction.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Assignment41
+{
+    public void ReverseStringWithoutReverseFunction()
+    {
+         Console.WriteLine("Enter A String:");
+         string str = Console.ReadLine();
+         string reverse = "";
+         for(int i = str.Length - 1; i>=0; i--){
+             reverse = reverse + str[i];
+         }
+        Console.WriteLine("Reverse Name:"+ reverse);
+
+    }
+}
