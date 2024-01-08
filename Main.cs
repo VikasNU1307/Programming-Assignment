f10c1318de232f4a0bb252ab4d963ed0c8536bbc
Vikas Umbadwar <vikasumbadwar@gmail.com>
2024-01-08 21:08:43 +0530

    main modifid

---
+++ Main.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Linq.Expressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Assignment42 obj = new Assignment42();
+        obj.StringLengthWithoutLibraryFunction();
+    }
+}
